4982e1dd793badd0824479b54905e3a7573027f3
rahul kharapkar <rahulkharapkar1122@gmail.com>
2020-06-08 21:02:41 +0530

    added audios to firebase

--- app/src/main/java/com/rahul/covid19/meditate_three.java
+++ app/src/main/java/com/rahul/covid19/meditate_three.java
@@ -7,17 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 
 public class meditate_three extends AppCompatActivity {
-    private MediaPlayer mMediaPlayer, mPlayer;
-    private Button mPlay, mPause, mP, mPa;
+    /*private MediaPlayer mMediaPlayer, mPlayer;
+    private Button mPlay, mPause, mP, mPa;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meditate_three);
 
-        mPlay = (Button) findViewById(R.id.play);
+      /*  mPlay = (Button) findViewById(R.id.play);
         mPause = (Button) findViewById((R.id.pause));
         mP = (Button) findViewById((R.id.button4));
         mPa = (Button) findViewById((R.id.button5));
@@ -51,6 +53,40 @@ public class meditate_three extends AppCompatActivity {
                 mMediaPlayer.pause();
             }
         });
+*/
+    }
+
+    public void play_song(View v) {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/covid19-66319.appspot.com/o/nintey_sec.mp3?alt=media&token=8bce4735-e551-44dd-8a48-fb513ae41447");
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void play_song_two(View v) {
+        MediaPlayer mediaPlay = new MediaPlayer();
+        try {
+            mediaPlay.setDataSource("https://firebasestorage.googleapis.com/v0/b/covid19-66319.appspot.com/o/nintey_sec.mp3?alt=media&token=8bce4735-e551-44dd-8a48-fb513ae41447");
+            mediaPlay.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            mediaPlay.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
